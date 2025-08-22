a07eca6e16b3ff0edd2129a20d9b813c68f52e5a
Natalia Uvarova <chibi-natachka@yandex.ru>
2025-08-23 01:27:36 +0300

    contact test added

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company,
+                 address, phone, home, mobile, work, fax, email, email2, email3,
+                 homepage):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phone = phone
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+
+
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from contact import Contact
 from group import Group
 
 
@@ -19,6 +20,18 @@ class TestAddGroup:
         self.create_group(driver, Group(name="", header="", footer=""))
         self.return_to_groups_page(driver)
         self.logout(driver)
+
+    def test_add_contact(self, driver):
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.create_contact(driver, Contact(first_name="aaa", last_name="bbb", mobile="89001111111",
+                                             middle_name="", nickname="", title="", company="",
+                                            address="", phone="", home="", work="", fax="", email="",
+                                            email2="", email3="",homepage=""
+                                            ))
+        self.go_to_homepage(driver)
+        self.logout(driver)
+
 
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
@@ -41,6 +54,84 @@ class TestAddGroup:
         driver.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         driver.find_element_by_name("submit").click()
+
+    def create_contact(self, driver, contact):
+        # init contact creation
+        driver.find_element_by_xpath(".//a[contains(@href, 'edit.php')]").click()
+        # fill contact form
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys(contact.first_name)
+
+        driver.find_element_by_name("middlename").click()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys(contact.middle_name)
+
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys(contact.last_name)
+
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("nickname").send_keys(contact.nickname)
+
+        driver.find_element_by_name("title").click()
+        driver.find_element_by_name("title").clear()
+        driver.find_element_by_name("title").send_keys(contact.title)
+
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys(contact.company)
+
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("address").send_keys(contact.address)
+
+        
+        # driver.find_element_by_name("?").click()
+        # driver.find_element_by_name("?").clear()
+        # driver.find_element_by_name("?").send_keys(contact.phone)
+        
+
+        driver.find_element_by_name("home").click()
+        driver.find_element_by_name("home").clear()
+        driver.find_element_by_name("home").send_keys(contact.home)
+
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("mobile").clear()
+        driver.find_element_by_name("mobile").send_keys(contact.mobile)
+
+        driver.find_element_by_name("work").click()
+        driver.find_element_by_name("work").clear()
+        driver.find_element_by_name("work").send_keys(contact.work)
+
+        driver.find_element_by_name("fax").click()
+        driver.find_element_by_name("fax").clear()
+        driver.find_element_by_name("fax").send_keys(contact.fax)
+
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys(contact.email)
+
+        driver.find_element_by_name("email2").click()
+        driver.find_element_by_name("email2").clear()
+        driver.find_element_by_name("email2").send_keys(contact.email2)
+
+        driver.find_element_by_name("email3").click()
+        driver.find_element_by_name("email3").clear()
+        driver.find_element_by_name("email3").send_keys(contact.middle_name)
+
+        driver.find_element_by_name("homepage").click()
+        driver.find_element_by_name("homepage").clear()
+        driver.find_element_by_name("homepage").send_keys(contact.homepage)
+
+        # submit group creation
+        driver.find_element_by_name("submit").click()
+        
+
+
+    def go_to_homepage(self, driver):
+        driver.find_element_by_xpath(".//a[contains(@href, './')]").click()
 
     def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
