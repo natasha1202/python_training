73e4a50d9fea6b2918e5ba9fe9ff66efb36d6f43
Natalia Uvarova <chibi-natachka@yandex.ru>
2025-08-24 21:55:14 +0300

    lesson 2 task 5

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,9 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="aaa", last_name="bbb", mobile="89001111111",
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(first_name="aaa", last_name="bbb", mobile="89001111111",
                                middle_name="", nickname="", title="", company="",
                                address="", phone="", home="", work="", fax="", email="",
                                email2="", email3="", homepage=""))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,12 +3,12 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="abd", header="abdc", footer="abdce"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="abd", header="abdc", footer="abdce"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
